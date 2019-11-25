8501a72792ffa951496294cdfa782928466f62db
Niloy Niil <22797857+niloyniil@users.noreply.github.com>
2019-11-25 15:42:21 +0600

    Add files via upload

---
+++ BFSqueue2/cmake-build-debug/CMakeFiles/BFSqueue2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BFSqueue2.dir/main.cpp.obj"
+  "BFSqueue2.pdb"
+  "BFSqueue2.exe"
+  "BFSqueue2.exe.manifest"
+  "libBFSqueue2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BFSqueue2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dfs/cmake-build-debug/CMakeFiles/dfs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dfs.dir/main.cpp.obj"
+  "dfs.pdb"
+  "dfs.exe"
+  "dfs.exe.manifest"
+  "libdfs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
