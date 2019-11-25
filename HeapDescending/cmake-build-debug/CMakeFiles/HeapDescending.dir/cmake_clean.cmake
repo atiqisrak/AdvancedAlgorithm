69bc52113a8cff4b541106a633804c86aa8f272f
Niloy Niil <22797857+niloyniil@users.noreply.github.com>
2019-11-25 15:47:08 +0600

    Add files via upload

---
+++ HeapDescending/cmake-build-debug/CMakeFiles/HeapDescending.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HeapDescending.dir/main.cpp.obj"
+  "HeapDescending.pdb"
+  "HeapDescending.exe"
+  "HeapDescending.exe.manifest"
+  "libHeapDescending.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HeapDescending.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ InsertionSort/cmake-build-debug/CMakeFiles/InsertionSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InsertionSort.dir/main.c.obj"
+  "InsertionSort.pdb"
+  "InsertionSort.exe"
+  "InsertionSort.exe.manifest"
+  "libInsertionSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/InsertionSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
