47d0d32c7bc95e8d402029951bd5fff999b6ebdf
Niloy Niil <22797857+niloyniil@users.noreply.github.com>
2019-11-25 15:49:42 +0600

    Add files via upload

---
+++ MergeSort/cmake-build-debug/CMakeFiles/MergeSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MergeSort.dir/main.c.obj"
+  "MergeSort.pdb"
+  "MergeSort.exe"
+  "MergeSort.exe.manifest"
+  "libMergeSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MergeSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ QuickSort/cmake-build-debug/CMakeFiles/QuickSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuickSort.dir/main.c.obj"
+  "QuickSort.pdb"
+  "QuickSort.exe"
+  "QuickSort.exe.manifest"
+  "libQuickSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuickSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
